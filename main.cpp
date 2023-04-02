8783a5956b9c294dcb2a20c9e1c5559888e88067
Abishev Daniyar <guarantee.excellence@gmail.com>
2023-04-02 13:31:46 +0600

    Last commit is tested.

--- main.cpp
+++ main.cpp
@@ -2,16 +2,40 @@
 
 #include "UserAccepter.h"
 
-const std::string IpAddress = "localhost";
+using namespace std;
+
+const string IpAddress = "127.0.0.1";
 const int Port = 44818;
 
 int main(int argc, char *argv[]) {
-	UserAccepter userAccpeter(&IpAddress, Port);
+	cout << "Initialize the socket..." << endl;
+
+	UserAccepter userAccepter(&IpAddress, Port);
+
+	if (userAccepter.getErrorState() < 0) {
+		cout << "Error occurred!" << endl;
+
+		return -1;
+	}
+
+	cout << "Accepting user connections..." << endl;
+
+	cout << flush;
 
 	User *userPtr;
 
-	while (userPtr = userAccpeter.acceptUser()) {
-		;
+	while (userPtr = userAccepter.acceptUser()) {
+		if (!userPtr) {
+			cout << "Error occurred!" << endl;
+
+			cout << flush;
+
+			break;
+		}
+
+		cout << "User Accepted!" << endl;
+
+		cout << flush;
 	}
 
 	return 0;
